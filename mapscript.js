ee8dc2908cb51b3161823073627cc624c0cbf0da
kaursimranjeet59@gmail.com <kaursimranjeet59@gmail.com>
2019-04-11 10:24:13 -0400

    lab1 and lab 2 commit

--- mapscript.js
+++ mapscript.js
@@ -21,4 +21,4 @@ function initialize()
     map2=new google.maps.Map(document.getElementById("map_canvas2"),mapOptions2);
     map3=new google.maps.Map(document.getElementById("map_canvas3"),mapOptions3);
 }
-  google.maps.event.addDomListener(Window,'load',initialize);
+  google.maps.event.addDomListener(window,'load',initialize);
